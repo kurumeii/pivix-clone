dc6cb8683ccca1a2e4df6ae690828c6647ec4653
kurumeii <babybangunny@gmail.com>
2022-12-16 22:09:10 +0700

    change back to System Environment Variables

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -5,7 +5,7 @@ import superjson from 'superjson'
 
 const getUrl = () => {
   if (typeof window === 'undefined') return ''
-  if (process.env.NEXTAUTH_URL) return `https://${process.env.NEXTAUTH_URL}`
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
 
