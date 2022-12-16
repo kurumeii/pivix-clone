cb9038ae7bf984e8a9081feb6b92d89b9159c219
kurumeii <babybangunny@gmail.com>
2022-12-16 23:25:27 +0700

    remove unused script

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -5,7 +5,7 @@ import superjson from 'superjson'
 
 const getUrl = () => {
   if (typeof window === 'undefined') return ''
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
 
@@ -22,5 +22,5 @@ export const trpc = createTRPCNext<AppRouter>({
       abortOnUnmount: true,
     }
   },
-  ssr: false,
+  ssr: true,
 })
